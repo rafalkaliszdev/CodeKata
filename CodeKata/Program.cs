d32fc72b06b8dea38922f9f6105c2039d62631a6
kvsanagi <119rafal@gmail.com>
2016-02-24 13:41:51 +0100

    1st upload
    24 feb

---
+++ CodeKata/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKata {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+    public static class Gaderypoluki {
+        public static Tuple<string, int[]> Encode(string key, string encodedText) {
+            key = key.ToLower(); //if case isn't changed, the key doesn't work
+            
+            if ((key.Length % 2) == 1) 
+                throw new ArgumentException("The number of letters in the encryption key must be even.");
+            
+            for (var a = 0; a < key.Length; ++a) {
+                for (var b = 0; b < key.Length; ++b) {
+                    if (a == b) {
+                        continue;
+                    }
+                    else if (key[a] == key[b]) {
+                        throw new ArgumentException("The encryption key is invalid. Each letter in the entire key must be unique.");
+                    }
+                }
+            }
+            
+            //initializes Set with pairs
+            SortedSet<Tuple<char, char>> tempCollection = new SortedSet<Tuple<char, char>>();
+            {
+                char first, second;
+                Tuple<char, char> pairedCharacters;
+                for (var a = 0; a < key.Length; a = a + 2) {
+                    first = key[a];
+                    second = key[a + 1];
+
+                    pairedCharacters = new Tuple<char, char>(first, second);
+                    tempCollection.Add(pairedCharacters);
+                }
+            }
+
+            var tupleCounter = 0;               //foreach tuple of set of six
+            var changesCounter = 0;             //whenever change occurs, it taps the counter
+            var array = new int[key.Length/2];  //array which stores number of replacements
+            var tempString = new StringBuilder(encodedText); //obligatory because can't change a string's character value throught index
+
+            //foreach below changes all occurrences mentioned in Encryption Key
+            //so, for 12 letters (6 pairs) in Key, it will iterate 6x times
+            foreach (Tuple<char, char> pair in tempCollection) {
+                changesCounter = 0; //reset counter
+
+                //for loop below goes throught a whole encodedText 
+                //for 200 character string it will iterate 200x
+                for (var a = 0; a < tempString.Length; ++a) {
+                    if (tempString[a] == pair.Item1) {  //if there is 'g'...
+                        tempString[a] = pair.Item2;     //...replace it with 'a'
+                        ++changesCounter; //notices every replacementsorted set
+                        continue;
+                    }
+                    if (tempString[a] == pair.Item2) {
+                        tempString[a] = pair.Item1;
+                        ++changesCounter; //notices every replacement
+                        continue;
+                    }
+                }
+                array[tupleCounter] = changesCounter; //if string is done, adds a number of replacements to array
+                ++tupleCounter; //and goes to next Tuple
+            }
+            encodedText = tempString.ToString(); //back to normal string
+            return new Tuple<string, int[]>(encodedText, array);
+        }
+    }
+}
---
+++ CodeKataTests/GaderypolukiTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKata;
+
+namespace CodeKata.Tests {
+    [TestClass]
+    public class GaderypolukiTests {
+        [TestMethod]
+        public void TestMethod1() {
+            var key = "GADERYPOLUKI";
+            var encodedText = "thd nlmbdy pf ydougcdmdnts sgvd kn thd gyygr kn thd knedx cpyydsopnekna tp thd sruugbud. thd knedxds mlst bd kn thd guohgbdtkcgu pyedy pf thd sruugbuds pf thd dncpekna idr, d.a. fpy thd idr 'odig' thd fkyst knedx wplue cpyydsopne tp 'ig' gne thd sdcpne knedx tp 'od' sruugbud.";
+            var expectedText = "the number of replacements save in the array in the index corresponding to the syllable. the indexes must be in the alphabetical order of the syllables of the encoding key, e.g. for the key 'peka' the first index would correspond to 'ka' and the second index to 'pe' syllable.";
+            Tuple<string, int[]> actual = Gaderypoluki.Encode(key, encodedText);
+            
+            Assert.AreEqual(expectedText.ToLower(),actual.Item1.ToLower());
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod2() {
+            var key = "GADERYPOLUK";
+            var encodedText = "thd nlmbdy pf ydougcdmdnts sgvd kn thd gyygr kn thd knedx cpyydsopnekna tp thd sruugbud. thd knedxds mlst bd kn thd guohgbdtkcgu pyedy pf thd sruugbuds pf thd dncpekna idr, d.a. fpy thd idr 'odig' thd fkyst knedx wplue cpyydsopne tp 'ig' gne thd sdcpne knedx tp 'od' sruugbud.";
+            try {
+                Gaderypoluki.Encode(key, encodedText);
+
+                Assert.Fail("Expected exception was not thrown.");
+            }
+            catch (ArgumentException ex) {
+                Assert.AreEqual("The number of letters in the encryption key must be even.", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod3() {
+            var key = "GADARYPOLUKI";
+            var encodedText = "thd nlmbdy pf ydougcdmdnts sgvd kn thd gyygr kn thd knedx cpyydsopnekna tp thd sruugbud. thd knedxds mlst bd kn thd guohgbdtkcgu pyedy pf thd sruugbuds pf thd dncpekna idr, d.a. fpy thd idr 'odig' thd fkyst knedx wplue cpyydsopne tp 'ig' gne thd sdcpne knedx tp 'od' sruugbud.";
+            try {
+                Gaderypoluki.Encode(key, encodedText);
+
+                Assert.Fail("Expected exception was not thrown.");
+            }
+            catch (ArgumentException ex) {
+                Assert.AreEqual("The encryption key is invalid. Each letter in the entire key must be unique.", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestMethod4() {
+            var key = "GAAERYPOLUKI";
+            var encodedText = "thd nlmbdy pf ydougcdmdnts sgvd kn thd gyygr kn thd knedx cpyydsopnekna tp thd sruugbud. thd knedxds mlst bd kn thd guohgbdtkcgu pyedy pf thd sruugbuds pf thd dncpekna idr, d.a. fpy thd idr 'odig' thd fkyst knedx wplue cpyydsopne tp 'ig' gne thd sdcpne knedx tp 'od' sruugbud.";
+            try {
+                Gaderypoluki.Encode(key, encodedText);
+
+                Assert.Fail("Expected exception was not thrown.");
+            }
+            catch (ArgumentException ex) {
+                Assert.AreEqual("The encryption key is invalid. Each letter in the entire key must be unique.", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod5() {
+            var key = "GADERYPOLUKI";
+            var encodedText = "thd nlmbdy pf ydougcdmdnts sgvd kn thd gyygr kn thd knedx cpyydsopnekna tp thd sruugbud. thd knedxds mlst bd kn thd guohgbdtkcgu pyedy pf thd sruugbuds pf thd dncpekna idr, d.a. fpy thd idr 'odig' thd fkyst knedx wplue cpyydsopne tp 'ig' gne thd sdcpne knedx tp 'od' sruugbud.";
+            Tuple<string, int[]> actual = Gaderypoluki.Encode(key, encodedText);
+
+            Assert.AreEqual(47, actual.Item2[0]);
+            Assert.AreEqual(16, actual.Item2[1]);
+            Assert.AreEqual(15, actual.Item2[2]);
+            Assert.AreEqual(16, actual.Item2[3]);
+            Assert.AreEqual(21, actual.Item2[4]);
+            Assert.AreEqual(18, actual.Item2[5]);
+        }
+    }
+}
+
